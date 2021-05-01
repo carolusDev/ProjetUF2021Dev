fe24b779748a6a4c9252a9016b0929021935f37c
carolusDev <clement.charlemagne@ynov.com>
2021-05-01 22:47:09 +0200

    pushez madame, les premieres vues arrivent!

---
+++ resources/views/index.blade.php
@@ -0,0 +1,43 @@
+@extends('app')
+
+@section('content')    
+<div class="ImHereForSpace"></div>
+<div class="contentContainer">
+    <div class="subContentContainer">
+        <h2 class="titlePresentation" >Présentation de l'entreprise</h2>
+        <hr class="subTitlePresentation" ></hr>
+        <p class="textPresentation">Depuis plus de 7 ans, On The Road A Game organise des voyages qui mêlent mystères, jeux, 
+            aventures et surtout rencontres humaines et culturelles. 
+            Le concept unique de On The Road A Game permet à ses participants de revenir aux origines 
+            du voyage : la découverte des autres.
+            Lorsqu’un voyageur se lance dans une session de On The Road a Game, il ne sait pas où il va. 
+            Aucune préparation ou anticipation possible. Il en sait juste assez pour savoir quel type de 
+            vêtements emporter dans son sac à dos…
+            Il s’agit donc d’un véritable voyage-mystère, couplé à un jeu entre équipes qui vient 
+            pimenter cette expérience hors du commun.</p>
+    </div>
+    <div class="subContentContainer">
+        <img class="presentationPicture" src="img/OTR-Entete.jpg" alt="on the road a game main picture"/>
+    </div>
+    <div class="subContentContainer">
+        <img class="presentationPicture" src="img/122217956_3437454263004745_427985025325149256_n.jpg" alt="key on wood table"/>
+    </div>
+    <div class="subContentContainer">
+        <h2 class="titlePresentation">Présentation de l'entreprise</h2>
+        <hr class="subTitlePresentation" ></hr>
+        <p class="textPresentation">On participe à On The Road a Game en équipe de 2 personnes, il n’y a pas d’élimination. 
+            Toutes les équipes sont assurées d’aller au bout de l’aventure et donc d’en profiter au 
+            maximum.
+            L’équipe gagnante est déterminée sur la base de défis à relever et d’un budget à (ne pas)
+            dépenser.
+            Au départ du jeu, une série de défis est proposée aux équipes. Le nombre de points
+            attribués dépend de la difficulté du défi. A elles, donc, de trouver le juste équilibre entre le
+            nombre et le degré de difficulté des défis qu’elles pensent pouvoir relever.
+            A cela, s’ajoute une gestion budgétaire. 
+            Chaque équipe démarre le jeu avec une enveloppe limitée. Et chaque Euro ramené à 
+            l’arrivée permet de décrocher des points bonus.
+            La somme excédentaire totale récoltée à l’issue du jeu est reversée à une association
+            caritative, choisie par l’équipe gagnante.</p>
+    </div>
+</div>
+@endsection
